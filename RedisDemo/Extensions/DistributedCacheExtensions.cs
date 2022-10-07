fd8cfd234f351650d0a8dd5638d304293d85e6c8
Krzysztof Juszcze <ep_kjuszcze@phreesia.com>
2022-10-07 13:34:27 +0200

    added caching

---
+++ RedisDemo/Extensions/DistributedCacheExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace RedisDemo.Extensions;
+
+public static class DistributedCacheExtensions
+{
+    public static async Task SetRecordAsync<T>(this IDistributedCache cache,
+        string recordId,
+        T data,
+        TimeSpan? absoluteExpireTime = null,
+        TimeSpan? unusedExpireTime = null)
+    {
+        var options = new DistributedCacheEntryOptions()
+        {
+            AbsoluteExpirationRelativeToNow = absoluteExpireTime ?? TimeSpan.FromMinutes(1),
+            SlidingExpiration = unusedExpireTime
+        };
+
+        var jsonData = JsonSerializer.Serialize(data);
+        await cache.SetStringAsync(recordId, jsonData, options);
+    }
+
+    public static async Task<T> GetRecordAsync<T>(this IDistributedCache cache, string recordId)
+    {
+        var recordJson = await cache.GetAsync(recordId);
+        if (recordJson is null)
+        {
+            return default(T);
+        }
+        return JsonSerializer.Deserialize<T>(recordJson);
+    }
+}
